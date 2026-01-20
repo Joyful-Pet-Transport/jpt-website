d98b57b53026284e2d73c9817494cdd19c47028a
Sezilla <chthollyc@gmail.com>
2026-01-20 22:07:32 +0800

    Update page.tsx

--- app/OurServices/page.tsx
+++ app/OurServices/page.tsx
@@ -1,3 +1,4 @@
+import BoxedContainer from "@/components/containers/BoxedContainer";
 import PageWrapperContainer from "@/components/containers/PageWrapperContainer";
 import BodyText from "@/components/elements/text/BodyText";
 import Heading from "@/components/elements/text/Heading";
@@ -5,13 +6,15 @@ import Heading from "@/components/elements/text/Heading";
 const OurServicesScreen = () => {
   return (
     <PageWrapperContainer className="items-center">
-      <div className="flex flex-col justify-center items-center">
-        <Heading className="text-center">Our Services</Heading>
-        <BodyText className="text-center">
-          Explore our pet relocation services with smooth, a walkthrough to our
-          service to make every journey safe and stress-free.
-        </BodyText>
-      </div>
+      <BoxedContainer>
+        <div className="flex flex-col justify-center items-center">
+          <Heading className="text-center">Our Services</Heading>
+          <BodyText className="text-center">
+            Explore our pet relocation services with smooth, a walkthrough to
+            our service to make every journey safe and stress-free.
+          </BodyText>
+        </div>
+      </BoxedContainer>
     </PageWrapperContainer>
   );
 };
