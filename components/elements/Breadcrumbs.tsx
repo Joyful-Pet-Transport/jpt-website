feab8ed0d2fd8dc535501614002936413fa29aba
Sezilla <chthollyc@gmail.com>
2026-02-13 22:49:30 +0800

    push ko muna baka mawala

--- components/elements/Breadcrumbs.tsx
+++ components/elements/Breadcrumbs.tsx
@@ -17,9 +17,17 @@ const Breadcrumbs: FC<BreadcrumbsProps> = ({ items }) => {
   const router = useRouter();
 
   return (
-    <div className="flex items-center justify-center gap-2 bg-white px-8 py-4 rounded-t-2xl mx-120 -mt-20 z-100">
+    <div
+      style={{
+        clipPath:
+          "path('M0 64 L34 64 A16 16 0 0 0 50 48 L50 16 A16 16 0 0 1 66 0 L534 0 A16 16 0 0 1 550 16 L550 48 A16 16 0 0 0 566 64 L600 64 Z')",
+        WebkitClipPath:
+          "path('M0 64 L34 64 A16 16 0 0 0 50 48 L50 16 A16 16 0 0 1 66 0 L534 0 A16 16 0 0 1 550 16 L550 48 A16 16 0 0 0 566 64 L600 64 Z')",
+      }}
+      className="z-100 h-16 w-[600px] flex self-center justify-center py-4 bg-white -mt-24"
+    >
       {items.map((item, index) => (
-        <div key={index} className="flex items-center gap-2">
+        <div key={index} className="flex gap-2">
           {item.path ? (
             <BodyText
               onPress={() => router.push(item.path!)}
