914b5c5842fe56b155a4eab49b8849bbb0ff4179
Sezilla <chthollyc@gmail.com>
2026-03-01 22:31:33 +0800

    added responsiveness for mobile

--- components/containers/GrayCardContainer.tsx
+++ components/containers/GrayCardContainer.tsx
@@ -1,8 +1,13 @@
 import { FC, PropsWithChildren } from "react";
+import { useIsMobile } from "@/utils/hooks/useWindowsDimensions";
 
 const GrayCardContainer: FC<PropsWithChildren> = ({ children }) => {
+  const mobile = useIsMobile();
+
   return (
-    <div className="rounded-xl bg-white justify-center items-center min-w-[500px] p-8">
+    <div
+      className={`rounded-xl bg-white justify-center items-center ${mobile ? "min-w-full" : "min-w-[500px]"} p-8`}
+    >
       {children}
     </div>
   );
--- components/sections/ContactUs.tsx
+++ components/sections/ContactUs.tsx
@@ -7,36 +7,15 @@ import InfoItemContainer from "../containers/InfoItemContainer";
 import IconContainer from "../containers/IconContainer";
 import DynamicButton from "../elements/button/DynamicButton";
 import InputBase from "../elements/input/TextInput/InputBase";
+import { useIsMobile } from "@/utils/hooks/useWindowsDimensions";
 
 const ContactUsSection: FC = () => {
+  const mobile = useIsMobile();
   const ContactUs: FC = () => {
     return (
-      <div className="grid lg:grid-cols-2 md:grid-cols gap-10">
-        <GrayCardContainer>
-          <div className="flex flex-col gap-8 items-center">
-            <div className="grid grid-cols-2 gap-4 w-full">
-              <InputBase label="First Name" placeholder="First name" />
-              <InputBase label="Last Name" placeholder="Last name" />
-            </div>
-            <InputBase
-              keyboardType="email"
-              widthFull
-              label="Email Address"
-              placeholder="E-mail address"
-            />
-            <InputBase
-              keyboardType="paragraph"
-              widthFull
-              label="Message"
-              placeholder="Your message..."
-            />
-            <DynamicButton size="medium" type="orange" className="w-full">
-              SEND MESSAGE
-            </DynamicButton>
-          </div>
-        </GrayCardContainer>
-        <div className="flex flex-1 flex-col gap-4">
-          <div>
+      <div className={`flex ${mobile ? "flex-col" : "flex-row"} gap-10 w-full`}>
+        <div className={`flex flex-col ${mobile ? "w-full" : "flex-1"}`}>
+          <div className={`mb-6 ${mobile ? "text-center" : ""}`}>
             <Heading font="fredoka">Feel free to contact us</Heading>
             <BodyText className="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -68,6 +47,33 @@ const ContactUsSection: FC = () => {
             />
           </div>
         </div>
+        <div className={mobile ? "w-full" : "flex-1"}>
+          <GrayCardContainer>
+            <div className="flex flex-col gap-8 items-center">
+              <div
+                className={`grid ${mobile ? "grid-cols-2" : "grid-cols-2"} gap-4 w-full`}
+              >
+                <InputBase label="First Name" placeholder="First name" />
+                <InputBase label="Last Name" placeholder="Last name" />
+              </div>
+              <InputBase
+                keyboardType="email"
+                widthFull
+                label="Email Address"
+                placeholder="E-mail address"
+              />
+              <InputBase
+                keyboardType="paragraph"
+                widthFull
+                label="Message"
+                placeholder="Your message..."
+              />
+              <DynamicButton size="medium" type="orange" className="w-full">
+                SEND MESSAGE
+              </DynamicButton>
+            </div>
+          </GrayCardContainer>
+        </div>
       </div>
     );
   };
@@ -77,8 +83,8 @@ const ContactUsSection: FC = () => {
       <div className="flex flex-1 w-full rounded-3xl overflow-hidden">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3865.1523149329405!2d120.97726777594258!3d14.360595786097761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xad761acc944e2d03%3A0xac2abd07f92c8d03!2sJoyful%20International%20and%20Domestic%20Pet%20Transport%20and%20Pet%20Care%20Services!5e0!3m2!1sen!2sph!4v1769349936524!5m2!1sen!2sph"
-          width="auto"
-          height="560"
+          width={mobile ? "100%" : ""}
+          height={mobile ? "400" : "560"}
           style={{ border: 0, flex: 1 }}
           allowFullScreen
           loading="lazy"
