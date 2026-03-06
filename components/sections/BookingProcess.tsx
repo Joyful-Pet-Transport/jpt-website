fc13a61de67081210c3d1db6a8e379f4ce7b56a0
Sezilla <chthollyc@gmail.com>
2026-03-06 09:30:43 +0800

    Update BookingProcess.tsx

--- components/sections/BookingProcess.tsx
+++ components/sections/BookingProcess.tsx
@@ -2,10 +2,33 @@ import { FC } from "react";
 import Heading from "../elements/text/Heading";
 import BookingProcessContent from "../contents/BookingProcessContent";
 import Image from "next/image";
-import { useIsMobile } from "@/utils/hooks/useWindowsDimensions";
+import { useIsMobile, useIsTablet } from "@/utils/hooks/useWindowsDimensions";
 
 const BookingProcessSection: FC = () => {
   const mobile = useIsMobile();
+  const tablet = useIsTablet();
+
+  if (tablet) {
+    return (
+      <div className="flex pb-16 flex-col items-start w-full">
+        <div className="px-16 flex flex-row items-end justify-between w-full relative">
+          <Heading font="fredoka" className="pb-8">
+            Our Joyful 4-Step Pet <br /> Transport Booking Process
+          </Heading>
+          <div>
+            <Image
+              src={"/images/element/bookingprocess.png"}
+              alt="Booking Process"
+              width={500}
+              height={500}
+              className="w-72 h-auto"
+            />
+          </div>
+        </div>
+        <BookingProcessContent />
+      </div>
+    );
+  }
 
   if (mobile) {
     return (
