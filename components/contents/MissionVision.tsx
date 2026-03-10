70300800ed51fb0f062654e2f50dac19e8986bd0
Sezilla <chthollyc@gmail.com>
2026-03-10 20:05:59 +0800

    fix

--- components/contents/MissionVision.tsx
+++ components/contents/MissionVision.tsx
@@ -9,23 +9,35 @@ const MissionVision: FC = () => {
         OUR MISSION & VISION
       </Heading>
       <div className="grid gap-8 md:grid-cols-2">
-        <div className="flex flex-col items-center justify-center bg-neutral-100 rounded-xl px-8 py-12 md:px-10 md:py-14 shadow-lg">
+        <div
+          style={{
+            background: "linear-gradient(to right, #FFB18C, #E1E1E3, #6EC9FF)",
+          }}
+          className="flex flex-col items-center justify-center bg-neutral-100 rounded-xl px-8 py-12 md:px-10 md:py-14 shadow-lg"
+        >
           <Heading weight="semibold" size="medium" className="pb-4 text-center">
             OUR MISSION
           </Heading>
           <BodyText className="text-center">
-            To provide safe, reliable, and compassionate pet relocation services while ensuring every pet travels 
-            comfortably and every furparent experiences a smooth and worry-free process.
+            To provide safe, reliable, and compassionate pet relocation services
+            while ensuring every pet travels comfortably and every furparent
+            experiences a smooth and worry-free process.
           </BodyText>
         </div>
 
-        <div className="flex flex-col items-center justify-center bg-neutral-100 rounded-xl px-8 py-12 md:px-10 md:py-14 shadow-lg">
+        <div
+          style={{
+            background: "linear-gradient(to right, #FFB18C, #E1E1E3, #6EC9FF)",
+          }}
+          className="flex flex-col items-center justify-center bg-neutral-100 rounded-xl px-8 py-12 md:px-10 md:py-14 shadow-lg"
+        >
           <Heading weight="semibold" size="medium" className="pb-4 text-center">
             OUR VISION
           </Heading>
           <BodyText className="text-center">
-            To become a trusted and recognized leader in pet transport services by delivering personalized, 
-            dependable, and high-quality relocation solutions for pets across the Philippines and around the world.
+            To become a trusted and recognized leader in pet transport services
+            by delivering personalized, dependable, and high-quality relocation
+            solutions for pets across the Philippines and around the world.
           </BodyText>
         </div>
       </div>
