f96a451ddb94e4ea8b7469c94dc579ad1d189019
Sezilla <chthollyc@gmail.com>
2026-03-14 14:37:49 +0800

    Update navItems.ts

--- utils/config/navItems.ts
+++ utils/config/navItems.ts
@@ -27,6 +27,6 @@ export const navItems: NavItem[] = [
   },
   { label: "Testimonials", path: "/testimonials" },
   { label: "About Us", path: "/about-us" },
-  { label: "Frequently Asked Questions", path: "/faq" },
+  { label: "FAQ", path: "/faq" },
   { label: "Contact Us", path: "/contact-us" },
 ];
